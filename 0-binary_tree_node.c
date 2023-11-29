7d3ba54d087dd233941f303f1e74552d77451379
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-11-29 02:29:12 -0800

    adding

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,9 +12,9 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *ptr = malloc(sizeof(binary_tree_t));
-	
+
 	if (!ptr)
-		return (NULL);;
+		return (NULL);
 
 	ptr->parent = parent;
 	ptr->n = value;
