f1e493cf0acfce9362dc55d7a3e17575b10dd6ec
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-11-29 04:10:42 -0800

    adding

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -12,26 +12,6 @@ void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return;
-	binary_tree_t *tmp1 = tree->left;
-	binary_tree_t *tmp2 = tree->right;
-	binary_tree_t *a;
-	binary_tree_t *b;
-
-	while (tmp1 != NULL || tmp2 != NULL)
-	{
-		if (tmp1 != NULL)
-		{
-			a = tmp1;
-			tmp1 = tmp1->left;
-			free(a);
-		}
-
-		if (tmp2 != NULL)
-		{
-			b = tmp2;
-			tmp2 = tmp2->right;
-			free(b);
-		}
-	}
+	free(tree);
 	return;
 }
