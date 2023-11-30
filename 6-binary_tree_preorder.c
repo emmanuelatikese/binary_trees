74a11745b849a6bf21031056a67c52655032fde7
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-11-29 20:19:25 -0800

    adding

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - This does the preorder list
+ *
+ * @tree: this the parent
+ * @func - This is a function pointer
+ *
+ * Return: returns NULL
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
