da88e405d2febbb5abf84e0f336973ca2052b058
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-12-01 03:03:40 -0800

    ading

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - This check the depth
+ * @tree: this is the node
+ *
+ * Returns: size_t or 0
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t c_depth = -1;
+
+		while(tree)
+		{
+			c_depth++;
+			tree = tree->parent;
+		}
+		return (c_depth);
+	}
+	return (0);
+}
