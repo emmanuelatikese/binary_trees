a966b58f87f85cbe40abf657f410f914248ff1ef
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-12-01 03:04:53 -0800

    adding

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -4,7 +4,7 @@
  * binary_tree_depth - This check the depth
  * @tree: this is the node
  *
- * Returns: size_t or 0
+ * Return: size_t or 0
  */
 
 size_t binary_tree_depth(const binary_tree_t *tree)
@@ -13,7 +13,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	{
 		size_t c_depth = -1;
 
-		while(tree)
+		while (tree)
 		{
 			c_depth++;
 			tree = tree->parent;
