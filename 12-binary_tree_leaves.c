d9c229711f26d8d1373ed2398bfdaea4343b2177
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-12-02 01:36:12 -0800

    adding

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -11,17 +11,11 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-		if (tree->parent == NULL && !tree->left && !tree->right)
-			return (0);
+		if (!tree->left && !tree->right)
+			return (1);
 		else
-		{
-			if (tree->left && tree->right)
-				return (2);
-			else if (tree->left || tree->right)
-				return (1);
-			else
-				return (1);
-		}
+			return binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+
 	}
 	return (0);
 }
