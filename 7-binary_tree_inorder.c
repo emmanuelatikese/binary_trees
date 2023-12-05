f76041fa62640a3347668a3f2336832e2405a01b
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-12-05 07:58:00 -0800

    adding files

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - This function prints the inorder order
+ * @tree: This is the first parameter
+ * @func: This is the function parameter
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree)
+	{
+		if (tree->left)
+			binary_tree_inorder(tree->left, func);
+
+		func(tree->n);
+
+		if (tree->right)
+			binary_tree_inorder(tree->left, func);
+	}
+}
